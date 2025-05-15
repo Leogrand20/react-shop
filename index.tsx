31598602977175f24eb32e8106720f1067412dcb
Denis Leonenko <leograndprix@gmail.com>
2025-05-15 14:16:55 +0300

    Renamed js => ts and jsx => tsx

--- index.tsx
+++ index.tsx
@@ -7,7 +7,7 @@ import { App } from './src/App'
 
 import '@css/index.css'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <App />

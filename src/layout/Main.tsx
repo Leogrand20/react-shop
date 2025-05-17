2fbfe41e72bc5ab85cd6065616e65ef66b771b91
Denis Leonenko <leograndprix@gmail.com>
2025-05-17 14:50:15 +0300

    basletlists

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -5,7 +5,7 @@ import { useShop } from '../context/useShop'
 
 import { GoodsList } from '../components/GoodsList'
 import { Cart } from '../components/Cart'
-import { BasketList } from '../components/BasketList'
+import { BasketList } from '../components/BasketLists'
 import { Alert } from '../components/Alert'
 
 export const Main = () => {
